43cb326e4f6d08069286b387947ab622f5080713
mk-gg <76687256+mk-gg@users.noreply.github.com>
2024-06-14 17:31:33 +0800

    Update sus.py
    fixed wrong guild name passed in create_panel

--- src/cogs/sus.py
+++ src/cogs/sus.py
@@ -413,11 +413,11 @@ class Sus(commands.Cog):
                                     if guild_name:
                                         
                                         if is_scam_server(guild_name):
-                                            create_panel(final_url, "Scam Server", guild_obj.name,  data['message'])
+                                            create_panel(final_url, "Scam Server", guild_name,  data['message'])
                                             
                                             await self.global_ban_user(user_id, data_guild_id)
                                             print(f"{time_format} {guild_format} [red]Banning user[/] {data['user_id']}")
-                                            await self.ban_user(data, "Scam Bio Link")
+                                            await self.ban_user(data, "Scam Attempt")
                                          
                                         
                     event_handlers['join']()
